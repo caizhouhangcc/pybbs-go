3b2112d972e89bb6bc54e5e0ec5e5ae2dd25e87d
Jingwei <zhjw43@163.com>
2018-09-01 12:24:45 +0800

    refactor markdown

--- utils/StrUtil.go
+++ utils/StrUtil.go
@@ -3,5 +3,7 @@ package utils
 import "strings"
 
 func NoHtml(str string) string {
-	return strings.Replace(strings.Replace(str, "<script", "&lt;script", -1), "script>", "script&gt;", -1)
+	strScriptLeft := strings.Replace(str, "<script", "&lt;script", -1)
+	strScriptRight := strings.Replace(strScriptLeft, "script>", "script&gt;", -1)
+	return strings.Replace(strScriptRight, "\r\n", "\n", -1)
 }
